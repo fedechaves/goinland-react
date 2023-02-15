cb7320c2e4507486b0723e2e9e9992491ca9cfd6
fedechaves <federicochavesf@gmail.com>
2023-02-15 16:53:14 -0300

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -11,33 +11,35 @@ import './header.css';
 const Header = () => {
   return (
     <div className='header'>
-      <div className="headerList">
+      <div className="headerContainer">
+        <div className="headerList">
 
-        <div className="headerListItem">
-            <FontAwesomeIcon icon={faBed} />  
-            <span>Stays</span>
-        </div>
+          <div className="headerListItem">
+              <FontAwesomeIcon icon={faBed} />  
+              <span>Stays</span>
+          </div>
 
-        <div className="headerListItem">
-            <FontAwesomeIcon icon={faPlane} />
-            <span>Flights</span>
-        </div>
+          <div className="headerListItem">
+              <FontAwesomeIcon icon={faPlane} />
+              <span>Flights</span>
+          </div>
 
-        <div className="headerListItem">
-            <FontAwesomeIcon icon={faCar} />
-            <span>Car rentals</span>
-        </div>
-        
-        <div className="headerListItem">
-            <FontAwesomeIcon icon={faBed} />
-            <span>Attractions</span>
-        </div>
+          <div className="headerListItem">
+              <FontAwesomeIcon icon={faCar} />
+              <span>Car rentals</span>
+          </div>
+          
+          <div className="headerListItem">
+              <FontAwesomeIcon icon={faBed} />
+              <span>Attractions</span>
+          </div>
 
-        <div className="headerListItem">
-            <FontAwesomeIcon icon={faTaxi} />
-            <span>Airport taxis</span>
-        </div>
+          <div className="headerListItem">
+              <FontAwesomeIcon icon={faTaxi} />
+              <span>Airport taxis</span>
+          </div>
 
+        </div>
       </div>
     </div>
   )
