d2a64c51d2c3a3c830e2332fb0ce4c6f1fea052d
fedechaves <federicochavesf@gmail.com>
2023-02-27 18:21:27 -0300

    feat: featured list created

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import "./home.css";
 import NavBar from "../../components/navBar/NavBar";
 import Header from "../../components/header/Header";
+import Featured from '../../components/featured/Featured';
 
 const Home = () => {
   return (
     <div>
       <NavBar />
       <Header />
+      <div className="homeContainer">
+        <Featured />
+      </div>
     </div>
   )
 }
--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import NavBar from '../../components/navBar/NavBar';
 import Header from '../../components/header/Header';
 
+
 const List = () => {
   return (
     <div>
         <NavBar/>
         <Header type="list"/>
-        <div className="homeContainer"></div>
     </div>
   )
 }
