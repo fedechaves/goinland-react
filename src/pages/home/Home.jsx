acdeec0e8456b3dac76f6b2945f852e812c216a6
fedechaves <federicochavesf@gmail.com>
2023-02-28 16:44:23 -0300

    feat: new component propertyList

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import "./home.css";
 import NavBar from "../../components/navBar/NavBar";
 import Header from "../../components/header/Header";
 import Featured from '../../components/featured/Featured';
+import PropertyList from '../../components/propertyList/PropertyList';
 
 const Home = () => {
   return (
@@ -11,7 +12,10 @@ const Home = () => {
       <Header />
       <div className="homeContainer">
         <Featured />
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList />
       </div>
+      
     </div>
   )
 }
