16e600e0cf9dca8b5a0b2598c90b0594a1e42a0b
fedechaves <federicochavesf@gmail.com>
2023-03-01 16:29:46 -0300

    feat: Featured Properties

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,6 +4,7 @@ import NavBar from "../../components/navBar/NavBar";
 import Header from "../../components/header/Header";
 import Featured from '../../components/featured/Featured';
 import PropertyList from '../../components/propertyList/PropertyList';
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
 
 const Home = () => {
   return (
@@ -14,6 +15,8 @@ const Home = () => {
         <Featured />
         <h1 className="homeTitle">Browse by property type</h1>
         <PropertyList />
+        <h1 className="homeTitle">Browse by property type</h1>
+        <FeaturedProperties />
       </div>
       
     </div>
