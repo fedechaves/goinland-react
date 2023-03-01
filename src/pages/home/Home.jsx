f4c2032e980f20cd51aa2b4144352dccafa1db84
fedechaves <federicochavesf@gmail.com>
2023-03-01 16:30:54 -0300

    correction in home fp title

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
         <Featured />
         <h1 className="homeTitle">Browse by property type</h1>
         <PropertyList />
-        <h1 className="homeTitle">Browse by property type</h1>
+        <h1 className="homeTitle">Homes Guests Love</h1>
         <FeaturedProperties />
       </div>
       
