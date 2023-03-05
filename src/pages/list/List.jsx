695e1b2277de6349e69cd3a927cda392d47d3908
fedechaves <federicochavesf@gmail.com>
2023-03-04 23:16:13 -0300

    feat: display list hotels in progress

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import './list.css';
 import NavBar from '../../components/navBar/NavBar';
 import Header from '../../components/header/Header';
 
@@ -8,6 +8,14 @@ const List = () => {
     <div>
         <NavBar/>
         <Header type="list"/>
+        <div className="listContainer">
+          <div className="listWrapper">
+            <div className="listSearch">
+              <h1 className="lsTitle">List Search</h1>
+            </div>
+            <div className="listResult"></div>
+          </div>
+        </div>
     </div>
   )
 }
