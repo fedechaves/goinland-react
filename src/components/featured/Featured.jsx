0b404061492cdcface84895fc50a56bdd411120c
fedechaves <federicochavesf@gmail.com>
2023-03-20 19:02:06 -0300

    feat: done featured properties component

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -2,9 +2,7 @@ import "./featured.css";
 import useFetch from "../../hooks/useFetch.js"
 
 const Featured = () => {
-    const { data, loading, error } = useFetch("http://localhost:2121/api/hotels/countbycity?cities=berlin,madrid,london")
-
-    console.log(data)
+    const { data, loading, error } = useFetch("http://localhost:2121/api/hotels/countbycity?cities=berlin,madrid,london");
 
   return (
     <div className="featured">
@@ -21,7 +19,7 @@ const Featured = () => {
         </div>
 
         <div className="featuredItem">
-            <img src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o=" 
+            <img src="https://cf.bstatic.com/xdata/images/city/max500/690334.webp?k=b99df435f06a15a1568ddd5f55d239507c0156985577681ab91274f917af6dbb&o=" 
                 alt="" 
                 className="featuredImg" />
             <div className="featuredTitles">
@@ -31,7 +29,7 @@ const Featured = () => {
         </div>
 
         <div className="featuredItem">
-            <img src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o=" 
+            <img src="https://cf.bstatic.com/xdata/images/city/max500/689422.webp?k=2595c93e7e067b9ba95f90713f80ba6e5fa88a66e6e55600bd27a5128808fdf2&o=" 
                 alt="" 
                 className="featuredImg" />
             <div className="featuredTitles">
--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,10 +1,13 @@
 import "./navBar.css";
+import {Link} from "react-router-dom";
 
 const NavBar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
+        <Link to={"/"} style={{color: "inherit", textDecoration: "none"  }}>
         <span className="logo">Asuntour</span>
+        </Link>
         <div className="navItems">
           <button className="navButton">register</button>
           <button className="navButton">login</button>
