71d9b189aed19a98bbf2a6f46ed4bfdb37a4dedf
fedechaves <federicochavesf@gmail.com>
2023-03-20 15:16:53 -0300

    feat: featured with fetch

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -8,13 +8,15 @@ const Featured = () => {
 
   return (
     <div className="featured">
+        {loading ? ("loading please wait") : (
+        <>
         <div className="featuredItem">
             <img src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o=" 
                 alt="" 
                 className="featuredImg" />
             <div className="featuredTitles">
-                <h1>Dublin</h1>
-                <h2>123 properties</h2>
+                <h1>Berlin</h1>
+                <h2>{data[0]} properties</h2>
             </div>
         </div>
 
@@ -23,8 +25,8 @@ const Featured = () => {
                 alt="" 
                 className="featuredImg" />
             <div className="featuredTitles">
-                <h1>Dublin</h1>
-                <h2>123 properties</h2>
+                <h1>Madrid</h1>
+                <h2>{data[1] } properties</h2>
             </div>
         </div>
 
@@ -33,10 +35,10 @@ const Featured = () => {
                 alt="" 
                 className="featuredImg" />
             <div className="featuredTitles">
-                <h1>Dublin</h1>
-                <h2>123 properties</h2>
+                <h1>London</h1>
+                <h2>{data[2]} properties</h2>
             </div>
-        </div>
+        </div></>)}
     </div>
   )
 }
